dc1eb08523a11e9bb3c6b2fe52b652bac141aae0
Veronica Hing <veronica.m.hing@gmail.com>
2021-03-03 19:53:35 -0800

    styled portfolio section and updated hamburger-bar

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const toggleButton= document.querySelector(".toggle-button");
 const mobileNav = document.querySelector(".mobile-nav");
 const backdrop = document.querySelector(".backdrop");
+const aboutLink = document.querySelector(".about-link");
+const portfolioLink = document.querySelector(".portfolio-link");
+const contactLink = document.querySelector(".contact-link");
+
 
 toggleButton.addEventListener("click", function() {
     mobileNav.classList.add("open");
     backdrop.classList.add("open");
 });
 
-backdrop.addEventListener("click", function(){
+function closeSideBar() {
     mobileNav.classList.remove("open");
     backdrop.classList.remove("open");
-})
+}
+
+backdrop.addEventListener("click", closeSideBar);
+aboutLink.addEventListener("click", closeSideBar);
+portfolioLink.addEventListener("click", closeSideBar);
+contactLink.addEventListener("click", closeSideBar);
